9247cc1c3c75a6a29ef763b8f771b35583106b1c
morsal <50400348+morsalsadat@users.noreply.github.com>
2022-01-29 16:13:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
  
 def caesar(text, shift, direction):
-  if direction == 'encode':
-    encoded = ""
-    for letter in text:
-      if alphabet.index(letter) + shift > 26:
-        encoded += alphabet[alphabet.index(letter) - 26 + shift]
+      if direction == 'encode':
+            encoded = ""
+            for letter in text:
+                  if alphabet.index(letter) + shift > 26:
+                        shift = shift % 26
+                        encoded += alphabet[alphabet.index(letter) - 26 + shift]
+                  else:
+                        encoded += alphabet[alphabet.index(letter) + shift]
+            print(f"The {direction}d message is: {encoded}")
       else:
-        encoded += alphabet[alphabet.index(letter) + shift]
-    print(f"The {direction}d message is: {encoded}")
-  else:
-    decoded = ""
-    for letter in text:
-      if alphabet.index(letter) - shift < 0:
-        decoded += alphabet[alphabet.index(letter) + 26 - shift]
-      else:
-        decoded += alphabet[alphabet.index(letter) - shift]
-    print(f"The {direction}d message is: {decoded}")
-
+            decoded = ""
+            for letter in text:
+                  if alphabet.index(letter) - shift < 0:
+                        shift = shift % 26
+                        decoded += alphabet[alphabet.index(letter) + 26 - shift]
+                  else:
+                        decoded += alphabet[alphabet.index(letter) - shift]
+            print(f"The {direction}d message is: {decoded}")
+      
 caesar(text, shift, direction)
